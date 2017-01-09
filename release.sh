89fb41b4eabcf7170790fc8f7e2ab7400ab21b64
dud3z <therealdud3z@gmail.com>
2017-01-10 00:55:04 +0100

    Pre-create the release output directory

--- release.sh
+++ release.sh
@@ -9,6 +9,8 @@ MOZVER=`gitver current | sed 's/+.*//g'`
 # temporarily set the Mozilla format version string
 sed -i 's/"(dev)"/"'$MOZVER'"/g' src/manifest.json
 
+mkdir -p bin
+
 # for this to work the `WEB_EXT_API_KEY` and `WEB_EXT_API_SECRET` env variables must be set
 web-ext sign -s src/ -a bin/
 
